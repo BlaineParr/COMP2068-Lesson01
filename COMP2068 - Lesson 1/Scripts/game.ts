0766334d0ea5db64f0ddab76c57da23b28354864
BlaineParr <Blaine.parr@hotmail.com>
2015-01-08 13:34:05 -0500

    added basic structure for typescript

---
+++ COMP2068 - Lesson 1/Scripts/game.ts
@@ -0,0 +1,14 @@
+﻿class Player {
+    strength: number;
+    constructor() {
+        this.strength = 10;
+    }
+
+    jump() {
+        console.debug("You jumped!");
+    }
+}
+function main() {
+}
+var tom = new Player();
+tom.jump();
